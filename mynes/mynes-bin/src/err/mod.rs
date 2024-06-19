27c6ec748ccb4a87d11f6b219fc7b30f9d2aa197
Master.G <master-g@users.noreply.github.com>
2024-06-19 21:55:26 +0800

    build: skeleton

--- mynes/mynes-bin/src/err/mod.rs
+++ mynes/mynes-bin/src/err/mod.rs
@@ -2,16 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-	#[error("bad argument: {0}")]
-	BadArgument(String),
 	#[error("cannot open the specified file: {0}")]
 	Io(#[from] std::io::Error),
-	#[error("there was an error with JSON serialization/deserializatoin: {0}")]
-	Json(String),
-	#[error("the value was not found: {0}")]
-	ValueNotFound(String),
-	#[error("authentication error: {0}")]
-	Auth(String),
-	#[error("validate error: {0}")]
-	Validate(String),
 }
